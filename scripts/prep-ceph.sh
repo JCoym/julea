797d0360b67e77752460d10df958ea363c959451
Johannes Coym <johannes.coym@studium.uni-hamburg.de>
2020-09-11 14:35:22 +0200

    Update Ceph install and prep script

--- scripts/prep-ceph.sh
+++ scripts/prep-ceph.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+SELF_PATH="$(readlink --canonicalize-existing -- "$0")"
+SELF_DIR="${SELF_PATH%/*}"
+SELF_BASE="${SELF_PATH##*/}"
 BLUESTORE_DIR=$1
 BLKDEV=$2
+
+usage ()
+{
+    echo "Usage: ${SELF_BASE} BLUESTORE_DIR BLOCK_DEV"
+    exit 1
+}
+
+test -n "$1" || usage
+test -n "$2" || usage
 
 if [ ! -d "$BLUESTORE_DIR" ]; then
     mkdir "$BLUESTORE_DIR"
 fi
-if [ "$(ls -A $DIR)" ]; then
-    rm -rf "$BLUESTORE_DIR/*"
+if [ "$(ls -A $BLUESTORE_DIR)" ]; then
+    rm -r "$BLUESTORE_DIR"/*
 fi
 ln -s "$BLKDEV" "$BLUESTORE_DIR/block"
