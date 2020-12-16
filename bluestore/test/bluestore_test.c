95dd0984fcce502a96077a2b60f674781b4c1101
Johannes Coym <johannes.coym@studium.uni-hamburg.de>
2020-12-16 14:34:05 +0100

    Add asserts and stat to test

--- bluestore/test/bluestore_test.c
+++ bluestore/test/bluestore_test.c
@@ -1,5 +1,6 @@
 #include "julea_bluestore.h"
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,29 +17,48 @@ main(int argc, char** argv)
     if (access("/tmp/bluestore-test/mkfs_done", F_OK) == 0)
     {
         printf("mkfs skipped \n");
+
         int mtrt = julea_bluestore_mount(store);
         printf("mount returned %d \n", mtrt);
+        assert(mtrt == 0);
+
         coll = julea_bluestore_open_collection(store);
     }
     else
     {
         int mkrt = julea_bluestore_mkfs(store);
         printf("mkfs returned %d \n", mkrt);
+        assert(mkrt == 0);
+
         int mtrt = julea_bluestore_mount(store);
         printf("mount returned %d \n", mtrt);
+        assert(mtrt == 0);
+
         coll = julea_bluestore_create_collection(store);
     }
 
     julea_bluestore_create(store, coll, "test_object");
     int bw = julea_bluestore_write(store, coll, "test_object", 0, "Test Object Content", 19);
     printf("Bytes written: %d \n", bw);
-    char* readback = malloc(sizeof(char) * 21);
-    int br = julea_bluestore_read(store, coll, "test_object", 0, &readback, 20);
+    assert(bw == 19);
+
+    char* readback = malloc(sizeof(char) * 20);
+    int br = julea_bluestore_read(store, coll, "test_object", 0, &readback, 19);
     printf("Bytes read: %d \n", br);
-    printf(readback);
+    assert(br == 19);
+
+    struct stat *buf;
+    buf = malloc(sizeof(struct stat));
+    julea_bluestore_status(store, coll, "test_object", buf);
+    int size = buf->st_size;
+    printf("Stat size: %d \n", size);
+    assert(size == 19);
+
     julea_bluestore_delete(store, coll, "test_object");
 
     int umtrt = julea_bluestore_umount(store, coll);
     printf("umount returned %d \n", umtrt);
+    assert(umtrt == 0);
+
     return 0;
 }
