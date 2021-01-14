e689e70d4f14c08101511f95a3e1ac1c92225a93
Johannes Coym <johannes.coym@studium.uni-hamburg.de>
2021-01-14 15:10:15 +0100

    Change ceph install script to disable malloc_usable_size in rocksdb

--- scripts/install-ceph.sh
+++ scripts/install-ceph.sh
@@ -10,6 +10,7 @@ CEPH_DIR="$(get_directory "${SELF_DIR}/..")/dependencies/ceph"
 git clone --recursive --single-branch https://github.com/ceph/ceph.git "${CEPH_DIR}"
 cd "${CEPH_DIR}"
 ./install-deps.sh
+sed -i "s/  add_definitions(-DROCKSDB_MALLOC_USABLE_SIZE)/#  add_definitions(-DROCKSDB_MALLOC_USABLE_SIZE)/g" "${CEPH_DIR}/src/rocksdb/CMakeLists.txt"
 ./do_cmake.sh -DWITH_BLUESTORE=ON -DWITH_BLUEFS=ON -DWITH_TESTS=OFF -DWITH_RADOSGW=OFF -DWITH_MGR=OFF
 cd build
 make -j8
